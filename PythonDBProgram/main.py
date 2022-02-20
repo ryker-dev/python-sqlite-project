ee4bfebe64b68861611a150d7161530089d12313
ryker-dev <pizzuti.dev@gmail.com>
2022-02-20 02:27:28 +0200

    Main and db base

---
+++ PythonDBProgram/main.py
@@ -0,0 +1,28 @@
+import sqlite3
+import os
+
+DEBUG = True
+PATH = os.path.dirname(__file__)
+
+db = sqlite3.connect(PATH + "/db/db.sqlite")
+cur = db.cursor()
+
+def create_db():
+    try:
+        f = open(PATH + "/../SQL queries.sql", "r") ## Execution method taken from Topic 6: Python
+        commands = ""
+        for line in f.readlines():
+            commands+=line
+        cur.executescript(commands)
+    except sqlite3.OperationalError:
+        print("Database already exists, skipping")
+    except Exception as err:
+        print(err) 
+
+def main():
+    create_db()
+    db.close()        
+    return None
+
+if __name__ == "__main__":
+    main()
